979a85b55d29fd15d0412ff5e1e931dc64045468
Venkata SaiPavan kumar <saipavan.kumar@wavelabs.ai>
2023-03-17 13:27:20 +0530

    Eureka Server

---
+++ Eureka Server/amazon-shopping/src/test/java/com/example/eureaka/client/api/AmazonShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureaka.client.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
